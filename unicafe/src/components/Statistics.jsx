8dfedea63fd8ed2d23f4634ed4cc5d939593bbc3
minmyatthu <mmthu17mce@ytu.edu.mm>
2021-06-30 22:56:54 +0630

    exercise 1.9

--- unicafe/src/components/Statistics.jsx
+++ unicafe/src/components/Statistics.jsx
@@ -4,17 +4,29 @@ const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
   const average = (good - bad) / total;
   const positive = (good / total) * 100;
+  if (total === 0) {
+    return (
+      <>
+        <h3 className="text-2xl font-semibold mb-4">Statics</h3>
+        <hr />
+        <div className="my-4">
+          <div>No feedback given</div>
+        </div>
+      </>
+    );
+  }
   return (
     <>
       <h3 className="text-2xl font-semibold mb-4">Statics</h3>
       <hr />
       <div className="my-4">
-        <div>Good {good}</div>
-        <div>Neutral {neutral}</div>
-        <div>Bad {bad}</div>
+        <div>😃 Good {good}</div>
+        <div>😐 Neutral {neutral}</div>
+        <div>😠 Bad {bad}</div>
+        <hr className="my-4" />
         <div>All {total}</div>
-        {total !== 0 && <div>Average {average}</div>}
-        {total !== 0 && <div>Positive {positive}%</div>}
+        <div>Average {average}</div>
+        <div>Positive {positive}%</div>
       </div>
     </>
   );
