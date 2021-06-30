7c89c1ea9a03be339292f656333b05017a0e8331
minmyatthu <mmthu17mce@ytu.edu.mm>
2021-06-30 22:22:05 +0630

    exercise 1.6

--- unicafe/tailwind.config.js
+++ unicafe/tailwind.config.js
@@ -1,11 +1,20 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        rose: colors.rose,
+        gray: colors.trueGray,
+      },
+    },
   },
   variants: {
-    extend: {},
+    extend: {
+      backgroundColor: ['active'],
+    },
   },
   plugins: [],
-}
+};
