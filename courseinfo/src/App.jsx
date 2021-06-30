9628bd7926b81b1648fb8554824297891b7493b6
minmyatthu <mmthu17mce@ytu.edu.mm>
2021-06-30 20:43:00 +0630

    exercises 1.2

--- courseinfo/src/App.jsx
+++ courseinfo/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-import Content from './components/Content';
 import Header from './components/Header';
+import Content from './components/Content';
 import Total from './components/Total';
 
 const App = () => {
@@ -19,11 +19,14 @@ const App = () => {
     <>
       <Header title={course} />
 
-      <div>
-        <Content part={part1} exercises={exercises1} />
-        <Content part={part2} exercises={exercises2} />
-        <Content part={part3} exercises={exercises3} />
-      </div>
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
+      />
 
       <Total total={total} />
     </>
